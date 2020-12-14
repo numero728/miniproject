2a5d652b88b04786d40af84b832227adb8d1e47a
numero728 <numero728@naver.com>
2020-12-14 23:22:58 +0900

    cc

--- run.py
+++ run.py
@@ -1,10 +1,14 @@
 from flask import Flask, render_template, session
 from flask_socketio import SocketIO, emit
+import re
+import time
+import requests
 
-
+search_api='AIzaSyCcsjF9lEDadtt1C76PyvEnK2jfjLAjuxk'
 app=Flask(__name__)
 app.secret_key='miniproject'
-socketio=SocketIO(app,cors_allowed_origins='*', async_mode='threading')
+socketio=SocketIO(app,ping_timeout=60,ping_intervla=50,cors_allowed_origins='*', async_mode='threading')
+msg_pack=[]
 
 @socketio.on('connect')
 def connect():
@@ -14,7 +18,18 @@ def connect():
 def client_send_name_handler(data):
   print(dir(data))
   client_name=data['client_name']
-  emit('server_send_msg',{'user':'admin','msg':f'{client_name}님이 입장하셨습니다.'},broadcast=True)
+  msg=f'{client_name}님이 입장하셨습니다.'
+  emit('system_msg',{'user':'admin','msg':msg},broadcast=True)
+  msg_pack.append({'user':'admin','msg'})
+
+@socketio.on('client_send_msg')
+def client_send_msg_handler(data):
+  client_name=data['client_name']
+  msg=data['client_msg']
+  stock_code=re.match(r'([0-9]6)',msg).group(0)
+  emit('server_send_msg',{'user':client_name,'msg':msg, 'stock_code'=stock_code},broadcast=True)
+  time.sleep(1)
+
 
 @app.route('/chat')
 def chat():
