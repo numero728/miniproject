ed8794fef54dbedc004cd2ad5fc8722124b2b685
numero728 <numero728@naver.com>
2020-12-17 10:00:22 +0900

    ruu py

--- run.py
+++ run.py
@@ -1,7 +1,43 @@
-from flask import Flask, render_template, request, url_for
+from flask import Flask, render_template, request, url_for, session
 from db.db_query import *
+from flask_socketio import SocketIO, emit
+
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'miniproject'
+
+# socketio 통신 관련
+if True:
+  socketio = SocketIO(app, cors_allowed_origins='*', pingInterval=600000, pingTimeout=600000, async_mode='threading', manage_session=False)
+  msg_pack = []
+
+  # 2. 유저 접속 수신
+  @socketio.on('connect')
+  def connect():
+      print('유저 접속')
+
+  # 4. 유저 송신한 닉네임 수신 및 시스템 메시지 송신
+
+  @socketio.on('username')
+  def username(data):
+      username = data['username']
+      msg = f'{username}님이 입장하셨습니다.'
+      emit('system_msg', {'user': 'admin', 'msg': msg}, broadcast=True)
+
+  # 7. 유저 송신한 메시지 수신 및 중계
+
+  @socketio.on('c_send_msg')
+  def c_send_msg(data):
+      username = data['username']
+      msg = data['msg']
+      emit('s_send_msg', {'user': username, 'msg': msg}, broadcast=True)
+
+  # 9. 유저 이탈
+
+  @socketio.on('disconnect')
+  def disconnect():
+      print('유저 이탈')
+
 
 @app.route('/')
 def home():
